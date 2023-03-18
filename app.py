7cbbab8848a8bfbead25da4732b4be8f4f300da0
Chan Ming Han <chanminghan00@gmail.com>
2023-03-18 17:34:08 +0800

    feat!: initial commit

---
+++ app.py
@@ -0,0 +1,232 @@
+import json
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pickle
+from dotenv import load_dotenv
+import os
+
+
+
+
+load_dotenv() #load environment variables
+
+
+
+app = Flask(__name__)
+# env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+# app.config.from_object(env_config)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://'+os.getenv("MYSQL_USERNAME")+ ':' + os.getenv("MYSQL_PASSWORD") + '@localhost/homey_db'
+
+config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+app.config.from_object(config_type)
+
+
+db = SQLAlchemy(app)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo = True)
+Session = sessionmaker(bind = engine)
+session = Session()
+
+
+from models import Account, UserSavedProperty, Property
+
+# routes
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route("/createUser", methods=["POST"])
+def createUser():
+    # data = json.loads('{"userID": 12, "userName": "hello", "password": "1234", "email": "hello@gmail.com", "savedListings": 9, "address": "clementi"}')
+    data = request.get_json() #get json payload from the post req
+    new_row = Account(userID=data['userID'], userName=data['userName'], password=data['password'], email=data['email'], savedListings=data['savedListings'], address=data['address'])
+    db.session.add(new_row)
+    db.session.commit()
+
+    return "user created"
+
+@app.route("/viewUser/<int:user_id>", methods=["GET"])
+def viewUser(user_id):
+    if request.method == 'GET':
+        account = db.session.query(Account).filter_by(userID = user_id)
+
+        if account.first() is None:
+            return f"Account with userID {user_id} does not exist"
+        
+        else:
+
+            acc = account[0]
+
+            accout = acc.as_dict()
+            return json.dumps(accout)
+
+            # return json.dumps("userID: {acc.userID}, userName: {acc.userName}, password: {acc.password}, email: {acc.email}, savedListings: {acc.savedListings}, address: {acc.address}}")
+
+
+
+@app.route("/deleteUser/<int:userID>", methods=["GET"])
+def deleteUser(userID):
+    if request.method == 'GET':
+        account = Account.query.filter_by(userID=userID).first()
+        if account is None:
+            return f"Account with userID {userID} does not exist"
+        
+        else:
+            db.session.delete(account)
+            db.session.commit()
+
+            return f"Account with userID {userID} has been deleted"
+        
+    else: 
+        return "method not allowed"
+    
+@app.route("/updateUser/<int:user_id>", methods=["POST"])
+def updateUser(user_id): 
+    if request.method =="POST":
+        account = db.session.query(Account).filter_by(userID = user_id)
+        acc = account[0]
+        data = request.get_json()
+
+        acc.userID = data['userID']
+        acc.userName = data['userName']
+        acc.password = data['password']
+        acc.email = data['email']
+        acc.savedListings = data['savedListings']
+        acc.address = data['address']
+
+        db.session.commit()
+
+        if account.first() is None:
+            return "Account no exist"
+        else:
+            return "Account successfully updated!"
+    else: 
+        return "update failed"
+    
+
+@app.route("/createProperty", methods=["POST"])
+def createProperty():
+    # data = json.loads('{"id": 1, "clusterId": "1", "type": "RENT"}')
+    data = request.get_json() #get json payload from the post req
+    print(data)
+    if "type" not in data:
+        new_row = Property(id=data['id'], clusterId=data['clusterId'], type=PropertyType.RENT)
+    else:
+        new_row = Property(id=data['id'], clusterId=data['clusterId'], type=data["type"])
+    db.session.add(new_row)
+    db.session.commit()
+
+    return "property created"
+
+@app.route("/viewProperty/<int:prop_id>", methods=["GET"])
+def viewProp(prop_id):
+    if request.method == 'GET':
+        property = db.session.query(Property).filter_by(id = prop_id)
+
+        if property.first() is None:
+            return f"Property with PropID {prop_id} does not exist"
+        
+        else:
+
+            prop = property[0]
+            propout = prop.as_dict()
+
+            return json.dumps(propout)
+
+@app.route("/deleteProperty/<int:prop_id>", methods=["GET"])
+def deleteProperty(prop_id):
+    if request.method == 'GET':
+        property = Property.query.filter_by(id=prop_id).first()
+        if property is None:
+            return f"Property with Property_ID {prop_id} does not exist"
+        
+        else:
+            db.session.delete(property)
+            db.session.commit()
+
+            return f"Property with id {prop_id} has been deleted"
+        
+    else: 
+        return "method not allowed"
+
+@app.route("/updateProperty/<int:prop_id>", methods=["POST"])
+def updateProperty(prop_id): 
+    if request.method =="POST":
+        property = db.session.query(Property).filter_by(id = prop_id)
+        prop = property[0]
+        data = request.get_json()
+
+        prop.id = data['id']
+        prop.clusterId = data['clusterId']
+        prop.type = data['type']
+
+        db.session.commit()
+
+        if property.first() is None:
+            return "Property no exist"
+        else:
+            return "Property successfully updated!"
+    else: 
+        return "update failed"
+
+
+
+
+@app.route("/createUserSavedProperty", methods=["POST"])
+def createUserSavedProperty():
+    # data = json.loads({"userID" : "1","propertyId" : "1", "property": {"id": 12, "clusterId": "1", "type": "RENT"}})
+    data = request.get_json()
+    pickledProperty = pickle.dumps(data["property"])
+    new_row = UserSavedProperty(userID=data["userID"], propertyId=data["propertyId"], property=pickledProperty)
+    db.session.add(new_row)
+    db.session.commit()
+
+    return "usp created"
+
+@app.route("/viewUSP/<int:user_id>", methods=["GET"])
+def viewUSP(user_id):
+    if request.method == 'GET':
+        usp = db.session.query(UserSavedProperty).filter_by(userID = user_id).all()
+        if len(usp)==0:
+            return f"User with userID {user_id} has no saved listings"
+        
+        else:
+            listout = []
+            for savedListing in usp:
+                listout.append(savedListing.as_dict())
+            return json.dumps({"result": listout})
+
+@app.route("/deleteUSP/<int:user_id>/<int:prop_id>", methods=["GET"])
+def deleteUSP(user_id,prop_id):
+    if request.method == 'GET':
+        usp = UserSavedProperty.query.filter_by(userID = user_id, propertyId=prop_id).first()
+        if usp is None:
+            return f"USP does not exist"
+        
+        else:
+            db.session.delete(usp)
+            db.session.commit()
+
+            return f"USP has been deleted"
+        
+    else: 
+        return "method not allowed"
+    
+
+if __name__ == "__main__":
+    app.app_context().push()
+    db.create_all()
+    app.run(host='0.0.0.0', debug=True)
+
+
+
+#1) Check if user and property exists
+#1.1) If user doesn't exists, exit (NOT_FOUND)
+#1.2) if property doesn't exists, create new Property 
+#2) if both property and user exists, create a new UserSavedProperty
+#3) return all UserSavedProperty with matching userID
--- models.py
+++ models.py
@@ -1,18 +1,25 @@
-from project import db
+from sqlalchemy import JSON, Column, String, Integer, create_engine, Insert, select
+from app import db
+from sqlalchemy.types import PickleType, Enum
+from sqlalchemy.schema import PrimaryKeyConstraint
+import enum
+import pickle
 
-
-class PropertyType(str, db.enum.Enum):
+class PropertyType(str, enum.Enum):
     RENT = "rent"
     SALE = "sale"
 
+# models in the database are init here. make sure database parameters do not change!
+# if changing any of the models, drop the table on sqlworkbench first, then reinitialise them.
+# models
 class Account(db.Model): 
     __tablename__ = 'UserAccounts'
-    userID = db.Column('userID', db.Integer, primary_key=True, unique=True)
-    userName = db.Column('userName', db.String(100), unique=True)
-    password = db.Column('password', db.String(100))
-    email = db.Column('email', db.String(100))
-    savedListings = db.Column('savedListings', db.Integer)
-    address = db.Column('address', db.String(100))
+    userID = Column('userID', Integer, primary_key=True, unique=True)
+    userName = Column('userName', String(100), unique=True)
+    password = Column('password', String(100))
+    email = Column('email', String(100))
+    savedListings = Column('savedListings', Integer)
+    address = Column('address', String(100))
 
     def __init__ (self, userID, userName, password, email, savedListings, address):
         self.userID = userID
@@ -24,12 +31,12 @@ class Account(db.Model):
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-    
+
 class Property(db.Model):
     __tablename__ = 'Property'
-    id = db.Column('id', db.String(100), primary_key=True)
-    clusterId = db.Column('clusterId', db.String(100))
-    type = db.Column('type', db.Enum(PropertyType))
+    id = Column('id', String(100), primary_key=True)
+    clusterId = Column('clusterId', String(100))
+    type = Column('type', Enum(PropertyType))
     # UserSaved = Column('UserSaved', )
 
     def __init__(self, id, clusterId, type):
@@ -43,11 +50,11 @@ class Property(db.Model):
 
 class UserSavedProperty(db.Model):
     __tablename__ = 'savedListings'
-    userID = db.Column('userID', db.String(100))
-    propertyId = db.Column('propertyId', db.String(100))
-    property = db.Column('property', db.PickleType) 
-    __table_args__ = (db.
-        db.PrimaryKeyConstraint(userID, propertyId),
+    userID = Column('userID', String(100))
+    propertyId = Column('propertyId', String(100))
+    property = Column('property', PickleType) 
+    __table_args__ = (
+        PrimaryKeyConstraint(userID, propertyId),
         {},
     )
 
@@ -59,11 +66,12 @@ class UserSavedProperty(db.Model):
     def as_dict(self):
         dicout = {}
         for col in self.__table__.columns:
+
             if col.name=="userID":
                 dicout["userID"] = getattr(self, col.name)
             if col.name=="propertyId":
                 dicout["propertyId"] = getattr(self, col.name)
             if col.name=="property":
-                dicout["property"] = db.pickle.loads(getattr(self,col.name))
+                dicout["property"] = pickle.loads(getattr(self,col.name))
 
         return dicout
